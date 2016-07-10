7509a850244c4e4066e5535e078c3b6f58e319d6
Tomikland <tomikland@gmail.com>
2016-07-10 13:50:49 +0200

    kameramozgás

---
+++ Assets/Script/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	Vector3 menniri;
+	float sebi = 2f;
+	float gsebi = 2f;
+	public GameObject tilesGO;
+	public Tiles tScript;
+	// Use this for initialization
+
+	void Start () {
+		tScript = tilesGO.GetComponent<Tiles> ();
+		transform.position = new Vector3 (tScript.MapX / 2 - 0.5f, tScript.MapY / 2 - 0.5f, -5);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		menniri = transform.position;
+
+		menniri.x = Mathf.Clamp(menniri.x + Input.GetAxis ("Horizontal") * sebi * Time.deltaTime*Camera.main.orthographicSize,0,tScript.MapX-1);
+		menniri.y = Mathf.Clamp(menniri.y + Input.GetAxis ("Vertical") * sebi * Time.deltaTime*Camera.main.orthographicSize,0,tScript.MapY-1);
+
+
+		transform.position = menniri; 
+
+		Camera.main.orthographicSize = Mathf.Clamp (Camera.main.orthographicSize + -Input.GetAxis ("Mouse ScrollWheel") * gsebi * Time.deltaTime*Mathf.Pow(Camera.main.orthographicSize,2),1,10);
+	}
+
+}
---
+++ Assets/Script/Tiles.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiles : MonoBehaviour {
+	public GameObject tilePrefab;
+	public int MapX = 10;
+	public int MapY = 10;
+	// Use this for initialization
+	void Start () {
+		GameObject go;
+		for (int x = 0; x < MapX; x++) {
+			for (int y = 0; y < MapY; y++) {
+				go = (GameObject)Instantiate (tilePrefab, new Vector3 (x, y,0),Quaternion.identity);
+				go.transform.parent = gameObject.transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
