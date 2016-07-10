0276a35148d120241cb3b00bea1c03fbeb07df13
Tomikland <tomikland@gmail.com>
2016-07-10 14:47:48 +0200

    Felsorolások

---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile  {
+
+	public int TileX;
+	public int TileY;
+	public Tile[] szomik = new Tile[8];
+	public GameObject TileGO;
+	public bool lathato = false;
+	public enum talajfajta {föld, üres, vas};
+	talajfajta eztalajfajta;
+
+	public Tile(int x, int y, GameObject go){
+		TileX = x;
+		TileY = y;
+		TileGO = go;
+	}
+
+}
